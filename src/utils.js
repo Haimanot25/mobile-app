0f445cf665a21ad941f3af3901abe7da0ef80bcb
Haimanot25 <Haimanot25@gmail.com>
2026-02-17 14:48:00 +0300

    Update README with setup instructions

--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,4 @@
 // Add unit tests for auth module - 1784576515
 // Add environment config template - 1784576519
 // Optimize image loading - 1784576520
+// Update README with setup instructions - 1784576522
